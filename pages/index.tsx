0ebbd1ac94a9ef31900435685dacbc633f06179c
ErickCu <erick.cuenca.cl@gmail.com>
2023-04-17 10:19:52 -0500

    estilos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header/header'
 
 import Styles from '/styles/Home.module.css'
-import { motion } from "framer-motion"
+import { Suspense, useState } from "react";
+import { motion, MotionConfig, useMotionValue } from "framer-motion";
+import { Shapes } from "./Shapes";
+import { transition } from "./settings";
+import useMeasure from "react-use-measure";
+
 import Link from 'next/link'
 
 
@@ -26,8 +31,21 @@ const item = {
 
 export default function Home() {
 
+  const [ref, bounds] = useMeasure({ scroll: false });
+  const [isHover, setIsHover] = useState(false);
+  const [isPress, setIsPress] = useState(false);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const resetMousePosition = () => {
+    mouseX.set(0);
+    mouseY.set(0);
+  };
+
+
 
   return (
+
 
     <>
 
@@ -36,6 +54,61 @@ export default function Home() {
         <section>
           {/* <h1>Bienvenidos a mi sitio web</h1> */}
           <br></br>
+          <MotionConfig transition={transition}>
+            <motion.button className={Styles.button}
+              ref={ref}
+              initial={false}
+              animate={isHover ? "hover" : "rest"}
+              whileTap="press"
+              variants={{
+                rest: { scale: 1 },
+                hover: { scale: 1.5 },
+                press: { scale: 1.4 }
+              }}
+              onHoverStart={() => {
+                resetMousePosition();
+                setIsHover(true);
+              }}
+              onHoverEnd={() => {
+                resetMousePosition();
+                setIsHover(false);
+              }}
+              onTapStart={() => setIsPress(true)}
+              onTap={() => setIsPress(false)}
+              onTapCancel={() => setIsPress(false)}
+              onPointerMove={(e) => {
+                mouseX.set(e.clientX - bounds.x - bounds.width / 2);
+                mouseY.set(e.clientY - bounds.y - bounds.height / 2);
+              }}
+            >
+              <motion.div
+                className="shapes"
+                variants={{
+                  rest: { opacity: 0 },
+                  hover: { opacity: 1 }
+                }}
+              >
+                <div className="pink blush" />
+                <div className="blue blush" />
+                <div className="container">
+                  <Suspense fallback={null}>
+                    <Shapes
+                      isHover={isHover}
+                      isPress={isPress}
+                      mouseX={mouseX}
+                      mouseY={mouseY}
+                    />
+                  </Suspense>
+                </div>
+              </motion.div>
+              <motion.div
+                variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
+                className="label"
+              >
+                play
+              </motion.div>
+            </motion.button>
+          </MotionConfig>
           <motion.div className={Styles.cardcontainer}
             variants={container}
             initial="hidden"
@@ -45,7 +118,7 @@ export default function Home() {
             >
               <Link href="/pantalla">
                 <br></br>
-                <h2>Card 1</h2>
+                <h2>Aventura Espacial</h2>
                 <motion.img src="/images/u8e8tuhh.png"
                   alt="Descripción de la imagen"
                   width={500}
@@ -55,7 +128,7 @@ export default function Home() {
                 />
               </Link>
 
-              <p>Esta es la descripción de la Card 2.</p>
+              <p>entra y vive una aventura especial</p>
               <br></br>
             </motion.div>
             <motion.div className={Styles.card}
@@ -63,7 +136,7 @@ export default function Home() {
               variants={item} >
               <Link href="/2pantalla">
                 <br></br>
-                <h2>Card 2</h2>
+                <h2>La Isla Pirata</h2>
                 <motion.img src="/images/calaca.png"
                   alt="Descripción de la imagen"
                   width={400}
@@ -88,7 +161,9 @@ export default function Home() {
                 <br></br>
               </motion.div> */}
           </motion.div>
+
         </section>
+
       </main>
 
 
--- pages/pantalla.tsx
+++ pages/pantalla.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header/header'
 import Styles from '/styles/pantalla.module.css'
 import { motion } from "framer-motion"
 
+
 const inter = Inter({ subsets: ['latin'] })
 const container = {
   hidden: { opacity: 0 },
@@ -22,28 +23,72 @@ const item = {
 
 
 export default function Home() {
+
+
+
   return (
     <>
+
       <Header />
       <div className={Styles.container}>
 
         <h1>hola que tal Bienvenido </h1>
 
-        <div className={Styles.cuerpo}>
 
-        <motion.img src="/images/u8e8tuhh.png"
-          alt="Descripción de la imagen"
-          width={500}
-          height={50}
-          layoutId="/images/u8e8tuhh.png"
-        />
+        <motion.div
 
-        <p> La tripulación de la nave espacial se encontraba a millones de kilómetros de casa, explorando un planeta desconocido. Todo parecía tranquilo hasta que detectaron una extraña señal en el horizonte. Intrigados, decidieron investigar y se encontraron con una civilización alienígena avanzada, con tecnología que superaba todo lo que habían visto antes. ¿Serían amigos o enemigos? La tripulación debía decidir si confiar en ellos o seguir adelante en su aventura espacial.</p>
+          animate={{ y: 20 }}
+          transition={{ ease: "easeOut", duration: 2 }}
+          initial={false}
 
-        </div>
+          className={Styles.cuerpo}>
 
+
+          <motion.img
+            src="/images/u8e8tuhh.png"
+            alt="Descripción de la imagen"
+            width={500}
+            height={50}
+            layoutId="/images/u8e8tuhh.png"
+          />
+
+          <p> La tripulación de la nave espacial se encontraba a millones de kilómetros de casa, explorando un planeta desconocido. Todo parecía tranquilo hasta que detectaron una extraña señal en el horizonte. Intrigados, decidieron investigar y se encontraron con una civilización alienígena avanzada, con tecnología que superaba todo lo que habían visto antes. ¿Serían amigos o enemigos? La tripulación debía decidir si confiar en ellos o seguir adelante en su aventura espacial.</p>
+
+
+
+        </motion.div>
+      </div>
+      <div className={Styles.botones}>
+        <motion.button
+          layout
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            opacity: { ease: "linear" },
+            layout: { duration: 5 }
+          }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className={Styles.boton}>
+
+
+          Sala De zoom</motion.button>
+        <motion.button layout
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            opacity: { ease: "backIn" },
+            layout: { duration: 5 }
+          }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className={Styles.boton}>
+          Mas informacion</motion.button>
 
       </div>
+
 
     </>
   );
