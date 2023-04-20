156e1642b00cb16a199d9c0b870bddba84bad070
ErickCu <erick.cuenca.cl@gmail.com>
2023-04-19 22:55:17 -0500

    nuevo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Styles from '/styles/Home.module.css'
 import { Suspense, useState } from "react";
 import { motion, MotionConfig, useMotionValue } from "framer-motion";
 // import { Shapes } from "./Shapes";
-import { transition } from "./settings";
+// import { transition } from "./settings";
 import useMeasure from "react-use-measure";
 
 import Link from 'next/link'
@@ -54,61 +54,6 @@ export default function Home() {
         <section>
           {/* <h1>Bienvenidos a mi sitio web</h1> */}
           <br></br>
-          <MotionConfig transition={transition}>
-            <motion.button className={Styles.button}
-              ref={ref}
-              initial={false}
-              animate={isHover ? "hover" : "rest"}
-              whileTap="press"
-              variants={{
-                rest: { scale: 1 },
-                hover: { scale: 1.5 },
-                press: { scale: 1.4 }
-              }}
-              onHoverStart={() => {
-                resetMousePosition();
-                setIsHover(true);
-              }}
-              onHoverEnd={() => {
-                resetMousePosition();
-                setIsHover(false);
-              }}
-              onTapStart={() => setIsPress(true)}
-              onTap={() => setIsPress(false)}
-              onTapCancel={() => setIsPress(false)}
-              onPointerMove={(e) => {
-                mouseX.set(e.clientX - bounds.x - bounds.width / 2);
-                mouseY.set(e.clientY - bounds.y - bounds.height / 2);
-              }}
-            >
-              <motion.div
-                className="shapes"
-                variants={{
-                  rest: { opacity: 0 },
-                  hover: { opacity: 1 }
-                }}
-              >
-                <div className="pink blush" />
-                <div className="blue blush" />
-                <div className="container">
-                  <Suspense fallback={null}>
-                    {/* <Shapes
-                      isHover={isHover}
-                      isPress={isPress}
-                      mouseX={mouseX}
-                      mouseY={mouseY}
-                    /> */}
-                  </Suspense>
-                </div>
-              </motion.div>
-              <motion.div
-                variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
-                className="label"
-              >
-                play
-              </motion.div>
-            </motion.button>
-          </MotionConfig>
           <motion.div className={Styles.cardcontainer}
             variants={container}
             initial="hidden"
