8c267f8a1b4238a4187fab9d9d1dcaec677dffbc
ErickCu <erick.cuenca.cl@gmail.com>
2023-06-16 08:55:56 -0500

    metodo get

--- pages/api/users.js
+++ pages/api/users.js
@@ -16,34 +16,34 @@ export default async function usersHandler(req, res) {
         } catch (error) {
             res.status(500).json({ error: 'Error al obtener los usuarios' });
         }
-    } else if (req.method === 'POST') {
-        try {
-            const { descripción, nombre, imagen } = req.body;
-            const newUser = { descripción, nombre, imagen };
-            // const docRef = await db.collection('Materias').addDoc(newUser);
-            const docRef = await addDoc(collection(db, 'mate'), { ...newUser })
-            const createdUser = { id: docRef.id, ...newUser };
-            res.status(201).json(createdUser);
-        } catch (error) {
-            res.status(500).json({ error: 'Error al crear el usuario' });
-        }
-    } else if (req.method === 'PUT') {
-        try {
-            // Actualizar un documento en la colección
-            const { id, descripción, nombre, imagen } = req.body;
-            await db.collection('mate').docs(id).update({
-                descripción, nombre, imagen
-            });
-            res.status(200).json({ id });
-        } catch (error) {
-            res.status(500).json({ error: 'User no actualizado' });
+    // } else if (req.method === 'POST') {
+    //     try {
+    //         const { descripción, nombre, imagen } = req.body;
+    //         const newUser = { descripción, nombre, imagen };
+    //         // const docRef = await db.collection('Materias').addDoc(newUser);
+    //         const docRef = await addDoc(collection(db, 'mate'), { ...newUser })
+    //         const createdUser = { id: docRef.id, ...newUser };
+    //         res.status(201).json(createdUser);
+    //     } catch (error) {
+    //         res.status(500).json({ error: 'Error al crear el usuario' });
+    //     }
+    //     } else if (req.method === 'PUT') {
+    //         try {
+    //             // Actualizar un documento en la colección
+    //             const { id, descripción, nombre, imagen } = req.body;
+    //             await db.collection('mate').docs(id).update({
+    //                 descripción, nombre, imagen
+    //             });
+    //             res.status(200).json({ id });
+    //         } catch (error) {
+    //             res.status(500).json({ error: 'User no actualizado' });
 
-        }
-    } else if (req.method === 'DELETE') {
-        // Eliminar un documento de la colección
-        const { id } = req.body;
-        await db.collection('mate').doc(id).delete();
-        res.status(200).json({ id });
+    //         }
+    //     } else if (req.method === 'DELETE') {
+    //         // Eliminar un documento de la colección
+    //         const { id } = req.body;
+    //         await db.collection('mate').doc(id).delete();
+    //         res.status(200).json({ id });
     } else {
         res.status(405).json({ error: 'Método no permitido' });
     }
