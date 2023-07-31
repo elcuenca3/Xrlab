1fb2a87e2bb8183897c4ff627a50f1ca7231cf56
ErickCu <erick.cuenca.cl@gmail.com>
2023-07-31 15:49:52 -0500

    3d

--- pages/context/MyContext.js
+++ pages/context/MyContext.js
@@ -1,16 +1,15 @@
-import React, { createContext, useContext, useState } from 'react';
+// import React, { createContext, useContext, useState } from 'react';
 
-export const MyContext = createContext();
+// export const MyContext = createContext();
 
-export const MyContextProvider = ({ children }) => {
-    const [dato, setDato] = useState('');
+// export const MyContextProvider = ({ children }) => {
+//     const [dato, setDato] = useState('');
 
-    return (
-        <MyContext.Provider value={{ dato, setDato }}>
-            {children}
-        </MyContext.Provider>
-    );
-};
+//     return (
+//         <MyContext.Provider value={{ dato, setDato }}>
+//             {children}
+//         </MyContext.Provider>
+//     );
+// };
 
-export const useMyContext = () => useContext(MyContext);
-a
+// export const useMyContext = () => useContext(MyContext);
